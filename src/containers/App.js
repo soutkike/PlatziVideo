396fd9955a764702f99291e1c13338f36db659b6
Enrique Castillo <enrique.castillo.itcm@gmail.com>
2021-06-09 00:54:17 -0500

    Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import "../assets/styles/App.scss";
 import Search from "../components/Search";
@@ -6,36 +7,52 @@ import Categories from "../components/Categorias";
 import Carrousel from "../components/Carrousel";
 import CarrouselItem from "../components/CarrouselItem";
 import Footer from "../components/Footer";
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
-    <Categories title="Mi Lista">
-      <Carrousel>
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-      </Carrousel>
-    </Categories>
+const App = () => {
+  const [videos, setVideos] = useState({
+    mylist: [],
+    trends: [],
+    originals: [],
+  });
 
-    <Categories title="Tendencias">
-      <Carrousel>
-        <CarrouselItem />
-        <CarrouselItem />
-      </Carrousel>
-    </Categories>
+  useEffect(() => {
+    fetch("http://localhost:3000/initialState")
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
 
-    <Categories title="Originales">
-      <Carrousel>
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-        <CarrouselItem />
-      </Carrousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+  console.log(videos);
+  console.log(videos.trends);
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+      {videos.mylist.length > 0 && (
+        <Categories title="Mi Lista">
+          <Carrousel>
+            <CarrouselItem />
+            <CarrouselItem />
+          </Carrousel>
+        </Categories>
+      )}
+
+      <Categories title="Tendencias">
+        <Carrousel>
+          {videos.trends?.map((item) => (
+            <CarrouselItem key={item.id} {...item} />
+          ))}
+        </Carrousel>
+      </Categories>
+
+      <Categories title="Originales">
+        <Carrousel>
+          {videos.originals?.map((item) => (
+            <CarrouselItem key={item.id} {...item} />
+          ))}
+        </Carrousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
